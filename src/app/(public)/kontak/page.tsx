315f52f1e75653281522ce08872685efead19361
Khaeruddin Asdar <46445417+Khaeruddinasdar12@users.noreply.github.com>
2025-11-23 08:05:19 +0800

    fix allowFullScreen issue

--- src/app/(public)/kontak/page.tsx
+++ src/app/(public)/kontak/page.tsx
@@ -159,7 +159,7 @@ export default function KontakPage() {
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4096.402837271695!2d120.30711280000001!3d-4.5415021!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbde5a92bb162a1%3A0xde65172229a002d5!2sGedung%20Fakuktas%20Syariah%20IAIN%20BONE!5e1!3m2!1sid!2sid!4v1763828520742!5m2!1sid!2sid"
                     className="absolute top-0 left-0 w-full h-full"
                     style={{ border: 0 }}
-                    allowFullScreen=""
+                    allowFullScreen
                     loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade">
                   </iframe>
